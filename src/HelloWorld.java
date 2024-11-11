21d2b5bf5e8baa19a95f4d413baea60c668314ac
Administrator <Administrator@172.31.14.187>
2024-11-11 06:32:12 +0000

    Java update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("This Java Hello world Project.");
+		System.out.println("This Java Hello world Project updated.");
 	}
 
 }
